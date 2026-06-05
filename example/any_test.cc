e0fc191e74b7d5140ecbffbb1916af20a4bbf4c8
WOODCOLA <2518172858@qq.com>
2026-06-05 22:12:20 +0800

    c++17 any

--- example/any_test.cc
+++ example/any_test.cc
@@ -2,6 +2,7 @@
 #include<string>
 #include<unistd.h>
 #include"any.hpp"
+#include<any>
 using namespace std;
 class task{
     public:
@@ -22,12 +23,18 @@ class task{
 };
 int main()
 {
-    Any a(string("aaaa"));
-    {
-        task b;
-        a = b;
-    }
-    for(;;)sleep(1);
+    std::any h;
+    h = 1111;
+    std::cout<<*std::any_cast<int>(&h)<<std::endl;
+    h = std::string("aaaaa");
+    std::cout<<*std::any_cast<std::string>(&h)<<std::endl;
+
+    // Any a(string("aaaa"));
+    // {
+    //     task b;
+    //     a = b;
+    // }
+    // for(;;)sleep(1);
     // std::cout<<*a.Get<std::string>()<<std::endl;
     // a = 10;
     // std::cout<<*a.Get<int>()<<std::endl;
