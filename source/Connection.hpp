7e82850d3eaa9bcfa5105ce89ca7902a5a51d000
WOODCOLA <2518172858@qq.com>
2026-06-09 18:58:32 +0800

    connection desgin

--- source/Connection.hpp
+++ source/Connection.hpp
@@ -9,7 +9,15 @@
 #include <memory>
 #include <functional>
 using namespace Buffer_Module;
-
+//1.状态
+//2.是否销毁
+//3.eventloop
+class EventLoop;
+typedef enum {DISCONNECTED,//表示连接关闭
+    CONNECTING ,//连接创建,可以进行处理的状态
+    CONNECTED,//连接成功,设置完成,可以通信
+    DISCONNECTING //表示连接准备关闭,仍然需要关心是否,输入缓冲区的数据释放处理,发送缓冲区的数据是否还要残留需要发送
+} CONN_STATUS;
 class Connection
 {
     using ConnPtr = std::shared_ptr<Connection>;
@@ -18,31 +26,118 @@ private:
     int _Conn_Id;  // 连接的唯一id ,方面后面查找管理
     int _Timer_Id; // 定时器id,我们需要添加定时器,需要唯一标识,这里用Conn id的值即可,因为只需要保证唯一性
     int _Sockfd;
+    CONN_STATUS _Status;// 当前连接的状态
+    bool Is_Enable_Time_del; //是否启用连接超时销毁
     Socket _Socket;    // 套接字管理
     Buffer _Inbuffer;  // 输入缓冲区
     Buffer _Outbuffer; // 输出缓冲区
-    Any _Any;          // 协议切换,上下文数据处理
-    using Conn_Read_Callback = std::function<void(const ConnPtr &)>;
+    Any _Context;          // 协议切换,上下文数据处理
+    EventLoop* _loop;  //一个连接(iofd)需要关联一个EventLoop(subreactor), 一个EventLoop需要关联一个线程 ,这个线程有整体的
+    //线程池处理,由masterreactor控制
+    using Conn_Connect_Callback = std::function<void(const ConnPtr &)>;
     using Conn_Write_Callback = std::function<void(const ConnPtr &)>;
     using Conn_Close_Callback = std::function<void(const ConnPtr &)>;
     using Conn_Event_Callback = std::function<void(const ConnPtr &)>;
     using Msg_Callback = std::function<void(const ConnPtr &, Buffer *)>; // 将数据放到输出缓冲区中(inbuffer) 业务处理
-    Conn_Read_Callback _Read_Cb;
+    Conn_Connect_Callback _Connect_Cb;
     Conn_Write_Callback _Write_Cb;
     Conn_Close_Callback _Close_Cb;
     Conn_Event_Callback _Event_Cb;
+     /*组件内的连接关闭回调-组件内设置的，因为服务器组件内会把所有的连接管理起来，⼀旦某个连接要关闭*/ 
+        /*就应该从管理的地方移除掉自己的信息*/ 
+    Conn_Close_Callback  _server_closed_callback;
     Msg_Callback _Msg_Cb;
+private:
+    void HanderRead()
+    {
+        //处理读事件
+        //从socket当中获取数据
+        //将数据放到inbuffer当中,调用读事件回调
+    }
+    void HanderWrite()
+    {
+        //处理写事件,调用写事件的回调
+        //将数据从输出缓冲区发送
+    }
+    void HanderClose()
+    {
 
-public:
-    Connection() {}
-    ~Connection() {}
-    void Send(char *data, ssize_t len);
-    void ShutDown();
-    void EnableTimeoutDel(int sec);
-    void DisableTimeoutDel();
-    void ChangeProtocal(const Conn_Read_Callback &Read_Cb,
+    }
+    void HanderEvent()
+    {
+
+    }
+    void HanderMsg()
+    {
+
+    }
+    void ReleaseInloop()
+    {
+        //实际的释放接口
+    }
+    void EstablishedInLoop();
+ void SendInLoop(char *data, ssize_t len);
+    void ShutDownInLoop();
+    void EnableTimeoutDelInLoop(int sec);
+    void DisableTimeoutDelInLoop();
+    void ChangeProtocalInLoop(const Any& Context,
+                        const Conn_Connect_Callback &Read_Cb,
                         const Conn_Write_Callback &Write_Cb,
                         const Conn_Close_Callback &Close_Cb,
                         const Conn_Event_Callback &Event_Cb,
                         const Msg_Callback &Msg_Cb);
+
+public:
+    Connection(int Conn_Id,int Sockfd,EventLoop* loop) 
+    :_Conn_Id(Conn_Id)
+    ,_Sockfd(Sockfd)
+    ,_loop(loop)
+    {}
+    ~Connection() {}
+    //获取conn_id
+    int Get_Id();
+    //获取sockfd
+    int Get_Fd();
+    //判断当前连接是否处于connect状态
+    bool Connected();
+    //获取上下文
+    Any* Get_Context();
+    //设置上下文
+    void Set_Context(const Any& context);
+    //发送数据
+    void Send(char *data, ssize_t len);
+    //开启超时销毁
+    void EnableTimeoutDel(int sec);
+      //关闭超时销毁
+    void DisableTimeoutDel();
+    void Shutdown();//给外部提供的关闭连接的接口,在这里并非真正上的关闭,仍然需要关心输入缓冲区的数据释放处理,发送缓冲区的数据是否还要残留需要发送
+    void Established();//当连接建立完成,我们需要给Channel ,设置各种回调,调用connect_callback,该函数也需要放到Eventloop当中实现
+    //切换协议
+    void ChangeProtocal(const Any& Context,
+                        const Conn_Connect_Callback &Connect_Cb,
+                        const Conn_Write_Callback &Write_Cb,
+                        const Conn_Close_Callback &Close_Cb,
+                        const Conn_Event_Callback &Event_Cb,
+                        const Msg_Callback &Msg_Cb);
+
+    void Set_Conn_Connect_Callback(const Conn_Connect_Callback&cb)
+    {
+
+    }
+    void Set_Conn_Write_Callback(const Conn_Write_Callback& cb)
+    {
+
+    }
+    void Set_Conn_Close_Callback(const Conn_Close_Callback& cb)
+    {
+
+    }
+    void Set_Conn_Event_Callback(const Conn_Event_Callback& cb)
+    {
+
+    }
+    void Set_Msg_Callback(const Msg_Callback& cb)
+    {
+
+    }
 };
